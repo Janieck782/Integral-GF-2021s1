c61cf757d4c9134aca322717ed0fd367ab90fd91
Janieck <52087242+Janieck782@users.noreply.github.com>
2021-07-07 01:49:06 -0400

    V 1.2 (Inserción de archivos completada)

--- js/coordenadas.js
+++ js/coordenadas.js
@@ -1,14 +1,14 @@
-class ubicacion{
-    constructor(t,n,x,y){
-        this.t = [];//p o c 
-        this.n = [];//id identificador
+class ubicacion {
+    constructor(t, n, x, y) {
+        this.t = []; //p o c 
+        this.n = []; //id identificador
         this.x = [];
         this.y = [];
     }
 }
 
-class demanda{
-    constructor(t,n,x,y){
+class demanda {
+    constructor(t, n, x, y) {
         this.c = [];
         this.p = [];
         this.n = [];
@@ -23,68 +23,72 @@ let dem = new demanda;
 let auxUbi;
 let auxDem;
 
-function guardarUbicaciones(){
+function guardarUbicaciones() {
     auxUbi = document.getElementById("coordenadas").value;
     registrarUbicaiones(auxUbi);
 }
 
-function registrarUbicaiones(aux){
-    
+function registrarUbicaiones(aux) {
+
     let txx = aux.split('\n');
-    let num = txx.length;  
+    let num = txx.length;
     let txs = [];
     let txt = [];
     let bep = [];
 
-    for(let i = 0 ; i < num ; i++){
-        console.log(txx[i]);
+    for (let i = 0; i < num; i++) {
+
         txs.push(txx[i]);
     }
 
-    for(let j = 0 ; j < txs.length; j++){
-        bep.push(txs[j].replace(',',';'));
+    for (let j = 0; j < txs.length; j++) {
+        bep.push(txs[j].replace(',', ';'));
         txt.push(bep[j].split(';'))
     }
 
-    for( let m = 0 ; m < txs.length; m++){
+    for (let m = 0; m < txs.length; m++) {
         ubi.t.push(txt[m][0]);
         ubi.n.push(txt[m][1]);
         ubi.x.push(Number.parseInt(txt[m][2]));
         ubi.y.push(Number.parseInt(txt[m][3]));
     }
 
-console.log(ubi);
 
 }
 
-function guardarDemanda(){
+function guardarDemanda() {
     auxDem = document.getElementById("demanda").value;
-    console.log(auxDem);
+    
     registrarDemanda(auxDem)
 }
 
-function registrarDemanda(aux){
+function registrarDemanda(aux) {
     let txx = aux.split('\n');
-    let num = txx.length;  
+    let num = txx.length;
     let txs = [];
     let txt = [];
     let bep = [];
 
-    for(let i = 0 ; i < num ; i++){
-        console.log(txx[i]);
+    for (let i = 0; i < num; i++) {
+        
         txs.push(txx[i]);
     }
 
-    for(let j = 0 ; j < txs.length; j++){
+    for (let j = 0; j < txs.length; j++) {
         txt.push(txx[j].split(';'))
     }
 
-    for( let m = 0 ; m < txs.length; m++){
+    for (let m = 0; m < txs.length; m++) {
         dem.c.push(txt[m][0]);
         dem.p.push(txt[m][1]);
         dem.n.push(Number.parseInt(txt[m][2]));
     }
 
+    console.log(ubi);
     console.log(dem);
+    
 }
 
+
+
+  
